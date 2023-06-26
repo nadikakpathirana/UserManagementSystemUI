18c9e849f53255a3a1934a5778675b9f3abce0f1
nadikakpathirana <nadikakpathirana@gmail.com>
2023-06-26 23:11:35 +0530

    Readme file updated.

--- src/utils/service/user-service.tsx
+++ src/utils/service/user-service.tsx
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 import Cookies from "universal-cookie";
 
 export class UserService {
-    static apiEndpoint: string = 'http://192.168.43.78:5223/api';
+    static apiEndpoint: string = 'http://localhost:5223/api';
 
     static login = (username: string, passwordHashed: string): Promise<AxiosResponse<User, any>> => axios.post<User>(`${this.apiEndpoint}/auth/login`, {username: username, password: passwordHashed});
 
